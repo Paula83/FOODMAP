c1d6d41336c3b6ad7d8c230551a8421b5149bc9f
Paula Andrea Giraldo <andreapaulagm@gmail.com>
2017-12-13 00:11:41 -0300

    Primera versión

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	setTimeout(function() {
+		$('.content').fadeOut(2000);
+    }, 2000); // <-- time in milliseconds
+
+
+
+	$('.category_item').click(function(){
+		var category = $(this).attr('id');
+
+
+		if(category == 'all'){
+			$('.comida_item').addClass('hide');
+			setTimeout(function(){
+				$('.comida_item').removerClass('hide');
+			}, 300);
+    } else {
+   	$('.comida_item').addClass('.hide');
+   	setTimeout(function(){
+				$('.' + category).removeClass('hide');
+			}, 300);
+   }
+   });
+});
+
+
+
